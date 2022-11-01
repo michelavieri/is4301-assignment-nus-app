a6a146f303521d64681997a5b8f719632af9330b
Michela Vieri <michela.vieri.hp@gmail.com>
2022-11-01 18:33:11 +0800

    feat: Developed Admission Page

--- src/pages/admissionPage.js
+++ src/pages/admissionPage.js
@@ -1,8 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function admissionPage() {
+    const AdmissionPageComponent = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 150px 60px 60px 60px;
+
+        h1 {
+            color: #4A4E69;
+        }
+    `
+
+    const ApplicationTimelineBox = styled.div`
+        background-color: #f5f2f0;
+        padding: 20px 40px;
+        display: flex;
+        justify-content: center;
+
+        ul {
+        li:not(:last-child) { 
+            margin-bottom: 20px;  
+         }
+        }
+    `
+
     return (
-        <div></div>
+        <AdmissionPageComponent>
+            <h1>Admission</h1>
+            <p>Here is the admission timeline:</p>
+            <ApplicationTimelineBox>
+                <ul>
+                    <li>
+                        Understanding Admission requirements
+                    </li>
+                    <li>
+                        Submit application online
+                    </li>
+                    <li>
+                        Upload supporting documents
+                    </li>
+                    <li>
+                        Make application fee payment
+                    </li>
+                    <li>
+                        Check application status
+                    </li>
+                </ul>
+            </ApplicationTimelineBox>
+        </AdmissionPageComponent>
     );
 }
 
